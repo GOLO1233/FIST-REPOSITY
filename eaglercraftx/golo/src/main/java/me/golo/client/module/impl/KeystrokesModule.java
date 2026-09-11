4a8a297ac44f2cc97be08256a3a901033f0f7cf9
GOLO1233 <sorokaaroslav557@gmail.com>
2026-09-11 11:37:26 +0000

    feat: add Keystrokes module scaffold

---
+++ eaglercraftx/golo/src/main/java/me/golo/client/module/impl/KeystrokesModule.java
@@ -0,0 +1,10 @@
+package me.golo.client.module.impl;
+
+import me.golo.client.module.Module;
+import me.golo.client.module.ModuleCategory;
+
+public final class KeystrokesModule extends Module {
+    public KeystrokesModule() {
+        super("Keystrokes", ModuleCategory.VISUAL);
+    }
+}
