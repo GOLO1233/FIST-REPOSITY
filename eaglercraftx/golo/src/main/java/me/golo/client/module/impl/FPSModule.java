717f8965aa22d40d7111400d4b992f7ed742a850
GOLO1233 <sorokaaroslav557@gmail.com>
2026-09-11 11:37:20 +0000

    feat: add FPS module scaffold

---
+++ eaglercraftx/golo/src/main/java/me/golo/client/module/impl/FPSModule.java
@@ -0,0 +1,10 @@
+package me.golo.client.module.impl;
+
+import me.golo.client.module.Module;
+import me.golo.client.module.ModuleCategory;
+
+public final class FPSModule extends Module {
+    public FPSModule() {
+        super("FPS", ModuleCategory.CLIENT);
+    }
+}
